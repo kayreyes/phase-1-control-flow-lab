153790cbe5f7d0776347bfa0b1444d0a1188844a
Kayla Reyes <kayla.reyes10.kr@gmail.com>
2023-04-16 23:22:31 -0500

    Completed phase-1-control-flow-lab

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let result;
+  if(ride<=400){
+    result = "This one is on me!";
+  }
+  if(ride>=400 && ride<=2000){
+    result = "That will be twenty bucks.";
+  }
+  if(ride>2000){
+    result = "I will gladly take your thirty bucks.";
+  }
+  if(ride>2500 ){
+    result="No can do.";
+  }
+  return result; 
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let result; 
+  if(city == 'NYC'){
+    result = "Ok, sounds good.";
+  }
+  if(city != 'NYC'){
+    result = "No go.";
+  }
+  return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let result;
+  
+  if(tip == 'not as generous'){
+    result = 'Thank you.';
+  }
+  else if(tip == 'generous'){
+    result = 'Thank you so much.';
+  }
+  else{
+    result = 'Bye.';
+  }
+  return result; 
 }
